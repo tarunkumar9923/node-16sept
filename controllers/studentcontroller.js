9994e60cb9973b7cbc10af9feb9955ec6f7ce35f
tarunkumar9923 <saintarun723@gmail.com>
2024-09-17 14:40:31 +0530

    updation

---
+++ controllers/studentcontroller.js
@@ -0,0 +1,29 @@
+// const mongoose = require('mongoose')
+const Student = require('../models/student')
+async function addStudent(req,res){
+    try{
+        console.log(req.body,'req.body')
+        let student = new Student(req.body);
+        await student.save();
+        console.log("data saved successfully")
+        res.end("Added")
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+async function getStudents(req,res){
+    try{
+        let students = await Student.find({});
+            console.log(students,'student');
+            res.send(students)
+
+        }
+    catch(err){
+
+    }
+}
+module.exports = {
+    addStudent,
+    getStudents
+}
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connection = require('./connection')
+const student = require('./routes/student')
+const app = express();
+connection();
+// student();
+app.use(express.json());
+app.use(student);
+app.listen(3000,(err)=>{
+    if(err){
+     console.log(err);
+    }else{
+        console.log('server is running on port 3000');
+    }
+})
+
+//http://localhost:3000/add/user
